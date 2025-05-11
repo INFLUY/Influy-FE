10a795180567ff6f2e4f842d99fdf3271ad671ad
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-11 17:53:43 +0900

    feat: default button common components

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,6 +4,31 @@ import type { SaveButtonProps } from './Button.types';
 export const SaveButton = ({
   onClick,
   disabled = true,
+  activeColor = 'bg-black',
+  disabledColor = 'bg-grey05',
+  text = '저장하기',
+  additionalStyles,
+}: SaveButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className={cn(
+        'body2-m flex h-[3.0625rem] w-[20.9375rem] items-center justify-center rounded-sm text-white',
+        disabled ? disabledColor : activeColor,
+        additionalStyles
+      )}
+    >
+      {text}
+    </button>
+  );
+};
+
+// flex-1,버튼 2개 나란히 있는 곳 용도. div(flex) 안에 버튼 2개 넣어서 사용
+export const DefaultButton = ({
+  onClick,
+  disabled = false,
   activeColor = 'bg-black ',
   disabledColor = 'bg-grey05 ',
   text = '저장하기',
@@ -15,7 +40,7 @@ export const SaveButton = ({
       onClick={onClick}
       disabled={disabled}
       className={cn(
-        'body2-m flex h-[3.0625rem] w-[20.9375rem] items-center justify-center rounded-sm text-white',
+        'body2-m flex h-[3.0625rem] flex-1 items-center justify-center rounded-sm text-white',
         disabled ? disabledColor + ' cursor-not-allowed' : activeColor,
         additionalStyles
       )}
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,4 @@ export { default as Loading } from './loading/Loading';
 export { ProductList, ProductGrid } from './user/Product';
 export { TimeChip, SoldOutChip, ExtendChip } from './user/Chip';
 
-export { SaveButton } from './common/Button';
+export { SaveButton, DefaultButton } from './common/Button';
