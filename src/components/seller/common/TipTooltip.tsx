afcb4f022fe0105c394e1d40f6a0f497d31e5a60
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-22 20:37:29 +0900

    design: tooltip 글 가운데 정렬

--- src/components/seller/common/TipTooltip.tsx
+++ src/components/seller/common/TipTooltip.tsx
@@ -1,8 +1,8 @@
 export const TipTooltip = ({ text }: { text: string }) => {
   return (
-    <div className="bg-grey02 caption-m flex w-full shrink-0 items-center justify-center gap-1 rounded-[.1875rem] px-2.5 py-2">
+    <div className="bg-grey02 caption-m flex w-full shrink-0 items-start justify-center gap-1 rounded-[.1875rem] px-2.5 py-2">
       <span className="text-black">Tip!</span>
-      <span className="text-grey08"> {text}</span>
+      <span className="text-grey08 text-center"> {text}</span>
     </div>
   );
 };
