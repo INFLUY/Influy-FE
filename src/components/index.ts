f7730411072cb0e6d880ba133b3b1cc79c57b203
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-22 15:01:34 +0900

    rename: seller/common으로 컴포넌트 이동

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,9 +7,6 @@ export { default as SellerProfileCard } from './common/SellerProfileCard';
 export { default as NoticeBanner } from './common/NoticeBanner';
 export { default as ExternalLinkChip } from './common/ExternalLinkChip';
 export { Tab, Tabs } from './common/Tab';
-export { SaveButton, DefaultButton } from './common/Button';
-export { TipTooltip } from './common/TipTooltip';
-export { LimitedTextInput, RegularTextInput } from './common/DetailInput';
 
 //user
 export { default as SellerProfileHeader } from './user/SellerProfileHeader';
@@ -17,4 +14,13 @@ export { ItemList, ItemGrid } from './user/Item';
 export { TimeChip, SoldOutChip, ExtendChip } from './user/Chip';
 
 //seller
+//common
+export { SaveButton, DefaultButton } from './seller/common/Button';
+export { TipTooltip } from './seller/common/TipTooltip';
+export {
+  LimitedTextInput,
+  RegularTextInput,
+} from './seller/common/DetailInput';
+export { default as CategoryChip } from './seller/common/CategoryChip';
+//item
 export { CategoryMultiSelector } from './seller/item/registration/CategoryMultiSelector';
--- src/components/seller/common/TipTooltip.tsx
+++ src/components/seller/common/TipTooltip.tsx
@@ -1,7 +1,4 @@
-interface TipTooltipProps {
-  text: string;
-}
-export const TipTooltip = ({ text }: TipTooltipProps) => {
+export const TipTooltip = ({ text }: { text: string }) => {
   return (
     <div className="bg-grey02 caption-m flex w-full shrink-0 items-center justify-center gap-1 rounded-[.1875rem] px-2.5 py-2">
       <span className="text-black">Tip!</span>
