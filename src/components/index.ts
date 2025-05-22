6994960f313aeea4ec4f2c9eb792d10b4919a830
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-22 09:53:45 +0900

    style: 파일 정리

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,22 +1,19 @@
 export { default as GlobalLayout } from './layout/GlobalLayout';
-export { default as GlobalLayout } from './layout/GlobalLayout';
-
 export { default as Loading } from './loading/Loading';
-export { default as BottomSheet } from './common/BottomSheet';
 
+// common
+export { default as BottomSheet } from './common/BottomSheet';
 export { default as SellerProfileCard } from './common/SellerProfileCard';
-export { default as SellerProfileHeader } from './user/SellerProfileHeader';
 export { default as NoticeBanner } from './common/NoticeBanner';
 export { default as ExternalLinkChip } from './common/ExternalLinkChip';
 export { Tab, Tabs } from './common/Tab';
+export { SaveButton, DefaultButton } from './common/Button';
+export { TipTooltip } from './common/TipTooltip';
+
+//user
+export { default as SellerProfileHeader } from './user/SellerProfileHeader';
 export { ItemList, ItemGrid } from './user/Item';
 export { TimeChip, SoldOutChip, ExtendChip } from './user/Chip';
-export { default as Loading } from './loading/Loading';
 
-export { ProductList, ProductGrid } from './user/Product';
-export { TimeChip, SoldOutChip, ExtendChip } from './user/Chip';
-
-export { SaveButton, DefaultButton } from './common/Button';
-
+//seller
 export { LimitedTextInput } from './seller/item/registration/DetailInput';
-export { TipTooltip } from './common/TipTooltip';
