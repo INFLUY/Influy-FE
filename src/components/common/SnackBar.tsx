ca2d178c5e0e6801a2411329b4a6064c28e40239
seoyeon5117 <seoyun020207@gmail.com>
2025-05-27 06:05:28 +0900

    fix: fix snackbar animation error

--- src/components/common/SnackBar.tsx
+++ src/components/common/SnackBar.tsx
@@ -11,30 +11,18 @@ const SnackBar = ({
   handleSnackBarClose: () => void;
 }) => {
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
-
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      handleSnackBarClose();
-    }, time);
+    const timer = setTimeout(() => {}, time);
     return () => clearTimeout(timer);
   }, [time, handleSnackBarClose]);
 
   return (
     <ModalPortal>
-      <div className="modal">
-        <div
-          className="modal-bg-layout bg-transparent"
-          onClick={handleSnackBarClose}
-        >
+      <div className="modal pointer-events-none">
+        <div className="modal-bg-layout bg-transparent">
           <div
-            className="bg-grey09 animate-fade-in-out absolute bottom-[6.5rem] flex items-center justify-center rounded-[.1875rem] px-4 py-[.625rem] text-white"
+            className="bg-grey09 animate-fade-in-out fixed bottom-[6.5rem] flex items-center justify-center rounded-[.1875rem] px-4 py-[.625rem] text-white"
             style={{ animationDuration: `${time}ms` }}
+            onAnimationEnd={() => handleSnackBarClose()}
             onClick={(e) => e.stopPropagation()}
           >
             {children}
