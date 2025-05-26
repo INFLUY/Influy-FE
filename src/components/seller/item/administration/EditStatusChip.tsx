2a9f679ef00f31ee61ad8fb2200f1bd5c8e74433
seoyeon5117 <seoyun020207@gmail.com>
2025-05-27 02:44:40 +0900

    feat: apply RadioBottomSheet to Status Settings

--- src/components/seller/item/administration/EditStatusChip.tsx
+++ src/components/seller/item/administration/EditStatusChip.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import EditIcon from '@/assets/icon/common/EditIcon.svg?react';
 
-const EditStatusChip = ({ children }: { children: string }) => {
+const EditStatusChip = ({
+  children,
+  onClick,
+}: {
+  children: string;
+  onClick: () => void;
+}) => {
   return (
     <div
       className="bg-grey03 text-grey09 caption-m inline-flex w-fit items-center justify-center gap-[.125rem] rounded-[.125rem] px-2 py-[.1875rem]"
-      onClick={() => {}}
+      onClick={onClick}
     >
       {children}
       <EditIcon className="text-grey06 h-[.875rem] w-[.875rem]" />
@@ -16,9 +22,11 @@ const EditStatusChip = ({ children }: { children: string }) => {
 export const EditTimeChip = ({
   open,
   deadline,
+  onClick,
 }: {
   open: string; // "2025-05-20T18:00:00"
   deadline: string; // "2025-05-20T18:00:00"
+  onClick: () => void;
 }) => {
   const [timeLeftUntilOpen, setTimeLeftUntilOpen] = useState<number>(0);
   const [timeLeftUntilClose, setTimeLeftUntilClose] = useState<number>(0);
@@ -48,11 +56,13 @@ export const EditTimeChip = ({
     if (!isToday(openTime)) {
       // 오픈 전 - D-n
       const dDay = Math.ceil(timeLeftUntilOpen / (1000 * 60 * 60 * 24));
-      return <EditStatusChip>{`D-${dDay}`}</EditStatusChip>;
+      return <EditStatusChip onClick={onClick}>{`D-${dDay}`}</EditStatusChip>;
     } else {
       // 오픈 당일 - 18:00 OPEN
       return (
-        <EditStatusChip>{`${openTime.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false })} OPEN`}</EditStatusChip>
+        <EditStatusChip
+          onClick={onClick}
+        >{`${openTime.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false })} OPEN`}</EditStatusChip>
       );
     }
   }
@@ -63,7 +73,7 @@ export const EditTimeChip = ({
     );
     // 오픈 ~ 마감 전 24시간 - NOW OPEN
     if (daysLeftUntilClose >= 1) {
-      return <EditStatusChip>NOW OPEN</EditStatusChip>;
+      return <EditStatusChip onClick={onClick}>NOW OPEN</EditStatusChip>;
     }
     // 마감 전 24시간 ~ 마감 - 23:59:59 LEFT
     else if (daysLeftUntilClose === 0 && timeLeftUntilClose > 0) {
@@ -77,13 +87,15 @@ export const EditTimeChip = ({
         (timeLeftUntilClose % (1000 * 60)) / 1000
       );
       return (
-        <EditStatusChip>{`${hoursLeftUntilClose.toString().padStart(2, '0')}:${minutesLeftUntilClose.toString().padStart(2, '0')}:${secondsLeftUntilClose.toString().padStart(2, '0')} LEFT`}</EditStatusChip>
+        <EditStatusChip
+          onClick={onClick}
+        >{`${hoursLeftUntilClose.toString().padStart(2, '0')}:${minutesLeftUntilClose.toString().padStart(2, '0')}:${secondsLeftUntilClose.toString().padStart(2, '0')} LEFT`}</EditStatusChip>
       );
     }
     // 마감 이후에는 칩이 뜨지 않음
   }
 };
 
-export const EditSoldOutChip = () => {
-  return <EditStatusChip>SOLDOUT</EditStatusChip>;
+export const EditSoldOutChip = ({ onClick }: { onClick: () => void }) => {
+  return <EditStatusChip onClick={onClick}>SOLDOUT</EditStatusChip>;
 };
--- src/components/seller/item/administration/SellerMyItem.tsx
+++ src/components/seller/item/administration/SellerMyItem.tsx
@@ -6,9 +6,26 @@ import {
 import KebabIcon from '@/assets/icon/common/KebabIcon.svg?react';
 import { useState } from 'react';
 import AdminItemBottomSheet from './AdminItemBottomSheet';
+import RadioBottomSheet from '@/components/seller/common/RadioBottomSheet';
+import { RadioInputList } from '@/components/seller/common/RadioInput.types';
 
 const SellerMyItem = ({ item }: { item: MyItem }) => {
   const [isEditItemOpen, setIsEditItemOpen] = useState<boolean>(false);
+  const [isStatusModalOpen, setIsStatusModalOpen] = useState<boolean>(false);
+
+  const STATUS_LIST: RadioInputList[] = [
+    {
+      id: 0,
+      text: '기본 자동 표기',
+      description:
+        '- 시작 전 : D-N 카운트, D-DAY는 오픈 시각 표기\n- 오픈~마감 24시간 전 : NOW OPEN\n- 마감 24시간 전~마감 : 23:59:59 LEFT\n- 연장 진행 시 : 기간 연장 표기\n  • 마감일 설정창에서 연장 표기 가능',
+    },
+    {
+      id: 1,
+      text: 'SOLD OUT 표기',
+      description: '마감 기한 전 완판 되었을 경우, 품절을 표기합니다.',
+    },
+  ];
 
   const handleEditMyItem = () => {
     setIsEditItemOpen(true);
@@ -38,18 +55,34 @@ const SellerMyItem = ({ item }: { item: MyItem }) => {
             {/* 제목 */}
             <h1 className="body2-m line-clamp-2">{item?.title}</h1>
             {/* 기간 */}
-            <p className="caption-m text-grey09 flex">2025.03.12~03.23(토)</p>
+            <p className="caption-m text-grey09 flex">
+              2025.03.12(수)~03.23(토)
+            </p>
+
             {/* 칩 */}
             <span className="caption-m text-grey07 flex items-center gap-1">
               상태 :
               {item?.status === 'sold out' ? (
-                <EditSoldOutChip />
+                <EditSoldOutChip onClick={() => setIsStatusModalOpen(true)} />
               ) : (
                 <span className="flex flex-wrap gap-1">
-                  <EditTimeChip open={item?.open} deadline={item?.deadline} />
+                  <EditTimeChip
+                    open={item?.open}
+                    deadline={item?.deadline}
+                    onClick={() => setIsStatusModalOpen(true)}
+                  />
                 </span>
               )}
             </span>
+            {/* 상품 상태 설정 모달 */}
+            {isStatusModalOpen && (
+              <RadioBottomSheet
+                title="현재 상태 설정"
+                list={STATUS_LIST}
+                isOpen={isStatusModalOpen}
+                setIsOpen={setIsStatusModalOpen}
+              />
+            )}
           </div>
           {/* 질문 정보 */}
           <span className="caption-m text-grey07 divide-grey05 flex gap-1 divide-x">
