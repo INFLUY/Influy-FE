3c043887876383dce967254149944d7de670e26e
seoyeon5117 <seoyun020207@gmail.com>
2025-05-29 01:35:08 +0900

    design: fix header escapin parent by changing fixed to sticky

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const PageHeader = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
   return (
-    <header className="text-grey10 subhead-sb border-grey03 fixed w-full items-center justify-center border-b px-5 py-[.875rem] text-center">
+    <header className="text-grey10 subhead-sb border-grey03 sticky w-full items-center justify-center border-b px-5 py-[.875rem] text-center">
       <ArrowIcon
         className="absolute top-1/2 h-6 w-6 -translate-y-1/2 cursor-pointer text-black"
         onClick={() => navigate(-1)}
