6a2c815b74e6c6701e34d44f50a8c928a06b6a56
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-05-30 09:26:50 +0900

    feat: form 적용

--- src/components/seller/item/registration/ItemForm.tsx
+++ src/components/seller/item/registration/ItemForm.tsx
@@ -15,43 +15,30 @@ import {
   TimePickerWheel,
   DateTimePicker,
   ItemSection,
+  FormWideTextArea,
+  FormLimitedTextInput,
 } from '@/components';
 import BottomSheet from '@/components/common/BottomSheet';
 import CalendarIcon from '@/assets/icon/common/Calendar.svg?react';
 import { useState, useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { ItemFormValues } from '@/types/item.types';
+import CheckBoxOffIcon from '@/assets/icon/common/CheckBox24On.svg?react';
+import CheckBoxOnIcon from '@/assets/icon/common/CheckBox24Off.svg?react';
 
 interface ItemFormProps {
   mode: 'create' | 'edit';
 }
 
 export const ItemForm = ({ mode }: ItemFormProps) => {
-  const {
-    register,
-    watch,
-    setValue,
-    formState: { errors },
-  } = useFormContext<ItemFormValues>();
-
-  //   const [titleText, setTitleText] = useState('');
-  //   const [linkText, setLinkText] = useState('');
-  //   const [commentText, setCommentText] = useState('');
   const [images, setImages] = useState<string[]>([]);
-  //   const [price, setPrice] = useState<number | undefined>(undefined);
-  //   const [salePrice, setSalePrice] = useState<number | undefined>(undefined);
-  //   const [period, setPeriod] = useState<number | null>(null);
-  //   const [selectedCategoryList, setSelectedCategoryList] = useState<string[]>(
-  //     []
-  //   );
+  const [isStartDateTimeSheetOpen, setIsStartDateTimeSheetOpen] =
+    useState(false);
+  const [isEndDateTimeSheetOpen, setIsEndDateTimeSheetOpen] = useState(false);
 
-  //   const [startDateTime, setStartDateTime] = useState<Date | null>(null);
-  //   const [endDateTime, setEndDateTime] = useState<Date | null>(null);
+  const { watch, setValue } = useFormContext<ItemFormValues>();
 
-  //   const [isStartDateTimeSheetOpen, setIsStartDateTimeSheetOpen] =
-  //     useState(false);
-  //   const [isEndDateTimeSheetOpen, setIsEndDateTimeSheetOpen] = useState(false);
-
+  const hasStartDate = watch('hasStartDate');
   const handleSaveClick = () => {};
 
   return (
@@ -77,9 +64,8 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
 
       {/* 제목 */}
       <ItemSection label="제목 *">
-        <LimitedTextInput
-          text={watch('titleText')}
-          setText={(val) => setValue('titleText', val)}
+        <FormLimitedTextInput
+          name="titleText"
           maxLength={40}
           placeHolderContent="상품의 이름을 입력해 주세요."
           isRequired={true}
@@ -87,13 +73,107 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
       </ItemSection>
 
       {/* 상품 카테고리 */}
-      {/* <ItemSection label="상품 카테고리 *">
-        <CategoryMultiSelector
-          selectedList={selectedCategoryList}
-          setSelectedList={setSelectedCategoryList}
+      <ItemSection
+        label="상품 카테고리 *"
+        tooltipText="1개 이상, 최대 3가지의 카테고리를 선택할 수 있습니다!"
+      >
+        <CategoryMultiSelector name="selectedCategoryList" />
+      </ItemSection>
+
+      {/* 기간 */}
+      <ItemSection label="기간 *">
+        <div className="flex w-full flex-col items-start gap-4">
+          {/* 시작일 */}
+          <div className="flex w-full flex-col items-start justify-center gap-1.5">
+            <p className="caption-m">시작일</p>
+            <div className="body2-m border-grey03 flex h-fit w-full items-center justify-between rounded-sm border px-3.5 py-2.5">
+              <span className="text-grey06">
+                달력에서 시작일과 시간을 선택하세요.
+              </span>
+              <CalendarIcon onClick={() => setIsStartDateTimeSheetOpen(true)} />
+            </div>
+          </div>
+          <div className="text-grey08 caption-m flex cursor-pointer items-center gap-[.375rem]">
+            {hasStartDate ? <CheckBoxOnIcon /> : <CheckBoxOffIcon />}
+            진행 중인 상품만 보기
+          </div>
+
+          {/* 마감일 */}
+          <div className="flex w-full flex-col items-start justify-center gap-1.5">
+            <p className="caption-m">마감일</p>
+            <SalePriceInput name="salePrice" />
+          </div>
+        </div>
+      </ItemSection>
+      {/* 날짜 바텀 시트 */}
+      {isStartDateTimeSheetOpen && (
+        <BottomSheet
+          onClose={() => setIsStartDateTimeSheetOpen(false)}
+          isBottomSheetOpen={isStartDateTimeSheetOpen}
+        >
+          <DateTimePicker
+            name="startISODateTime"
+            type="시작일"
+            onClose={() => setIsStartDateTimeSheetOpen(false)}
+          />
+        </BottomSheet>
+      )}
+
+      {/* 날짜 */}
+      {isEndDateTimeSheetOpen && (
+        <BottomSheet
+          onClose={() => setIsEndDateTimeSheetOpen(false)}
+          isBottomSheetOpen={isEndDateTimeSheetOpen}
+        >
+          <DateTimePicker
+            name="endISODateTime"
+            type="마감일"
+            onClose={() => setIsEndDateTimeSheetOpen(false)}
+          />
+        </BottomSheet>
+      )}
+
+      {/* 한 줄 소개 */}
+      <ItemSection
+        label="한 줄 소개 *"
+        tooltipText="제품의 특징을 간단하게 소개해 주세요!"
+      >
+        <FormLimitedTextInput
+          name="summaryText"
+          maxLength={18}
+          placeHolderContent="제품 한 줄 소개를 입력해 주세요."
+          isRequired={true}
         />
-        <TipTooltip text="첫 번째로 선택한 사진이 대표 사진으로 등록돼요!" />
-      </ItemSection> */}
+      </ItemSection>
+
+      {/* 가격 */}
+      <ItemSection label="가격 *">
+        <div className="flex w-full flex-col items-start gap-3">
+          <div className="flex w-full flex-col items-start justify-center gap-1.5">
+            <p className="caption-m">정가</p>
+            <PriceInput name="price" />
+          </div>
+          <div className="flex w-full flex-col items-start justify-center gap-1.5">
+            <p className="caption-m">할인가</p>
+            <SalePriceInput name="salePrice" />
+          </div>
+        </div>
+      </ItemSection>
+
+      {/* 판매 링크 */}
+      <ItemSection label="판매 링크">
+        <LinkInput name="linkText" />
+      </ItemSection>
+
+      {/* 진행 회차 */}
+      <ItemSection label="진행 회차">
+        <PeriodDropdown name={'period'} />
+      </ItemSection>
+
+      {/* COMMENT */}
+      <ItemSection label="COMMENT">
+        <FormWideTextArea name={'commentText'} />
+      </ItemSection>
     </section>
   );
 };
@@ -134,8 +214,7 @@ export const ItemRegistrationTest = () => {
           isBottomSheetOpen={isStartDateTimeSheetOpen}
         >
           <DateTimePicker
-            initialDateTime={startDateTime}
-            onSave={setStartDateTime}
+            name="startISODateTime"
             type="시작일"
             onClose={() => setIsStartDateTimeSheetOpen(false)}
           />
@@ -149,9 +228,8 @@ export const ItemRegistrationTest = () => {
           isBottomSheetOpen={isEndDateTimeSheetOpen}
         >
           <DateTimePicker
-            initialDateTime={endDateTime}
-            onSave={setEndDateTime}
-            type="시작일"
+            name="endISODateTime"
+            type="마감일"
             onClose={() => setIsEndDateTimeSheetOpen(false)}
           />
         </BottomSheet>
@@ -184,14 +262,6 @@ export const ItemRegistrationTest = () => {
         setText={setCommentText}
         placeHolderContent="제품 선택 이유, 특징, 사용 경험 등 제품의 매력을 보여줄 수 있는 내용을 자유롭게 작성해 주세요."
         isRequired={true}
-      />
-      <PeriodDropdown period={period} setPeriod={setPeriod} />
-      <PriceInput price={price} setPrice={setPrice} />
-
-      <SalePriceInput
-        regularPrice={price}
-        salePrice={salePrice}
-        setSalePrice={setSalePrice}
       />
 
       <TipTooltip
--- src/types/item.types.ts
+++ src/types/item.types.ts
@@ -1,9 +1,11 @@
 export interface ItemFormValues {
   titleText: string;
   selectedCategoryList: string[];
+  hasStartDate: boolean;
   startISODateTime: string | null;
+  hasEndDate: boolean;
   endISODateTime: string | null;
-  summaryText: string;
+  summaryText: string | null;
   price: number;
   salePrice: number;
   linkText: string | null;
