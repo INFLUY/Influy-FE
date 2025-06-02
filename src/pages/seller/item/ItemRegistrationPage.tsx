d818385771c6be941be269c8fee20cdf951c477a
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-06-03 04:02:02 +0900

    feat: 제출 형식

--- src/pages/seller/item/ItemRegistrationPage.tsx
+++ src/pages/seller/item/ItemRegistrationPage.tsx
@@ -58,7 +58,14 @@ export const ItemRegistrationPage = () => {
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(onSubmit)}>
         <ItemForm mode="create" />
-        <button type="submit">저장</button>
+        <article className="border-t-grey02 absolute bottom-0 left-0 flex h-24 w-full shrink-0 items-center justify-center gap-6 border-t border-solid bg-white px-5 pt-2.5 pb-2">
+          <DefaultButton onClick={onSave} text="보관하기" />
+          <DefaultButton
+            type="submit"
+            text="게시하기"
+            disabled={methods.formState.isSubmitting}
+          />
+        </article>
       </form>
     </FormProvider>
   );
