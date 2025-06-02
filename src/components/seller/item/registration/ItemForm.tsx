deff531c863f3696fbb0d94cd48b002522ca8ba9
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-06-03 03:02:25 +0900

    feat: date time picker zod

--- src/components/seller/item/registration/ItemForm.tsx
+++ src/components/seller/item/registration/ItemForm.tsx
@@ -4,15 +4,13 @@ import {
   LimitedTextInput,
   TipTooltip,
   CategoryMultiSelector,
-  LinkInput,
+  FormLinkInput,
   CategoryChip,
   WideTextArea,
   ItemImageUploader,
-  PriceInput,
-  SalePriceInput,
+  FormPriceInput,
+  FormSalePriceInput,
   PeriodDropdown,
-  DatePickerCalender,
-  TimePickerWheel,
   DateTimePicker,
   ItemSection,
   FormWideTextArea,
@@ -20,11 +18,11 @@ import {
 } from '@/components';
 import BottomSheet from '@/components/common/BottomSheet';
 import CalendarIcon from '@/assets/icon/common/Calendar.svg?react';
-import { useState, useEffect } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { useState, useEffect, use } from 'react';
+import { useFormContext, useController } from 'react-hook-form';
 import { ItemFormValues } from '@/types/item.types';
-import CheckBoxOffIcon from '@/assets/icon/common/CheckBox24On.svg?react';
-import CheckBoxOnIcon from '@/assets/icon/common/CheckBox24Off.svg?react';
+import CheckBoxOnIcon from '@/assets/icon/common/CheckBox24On.svg?react';
+import CheckBoxOffIcon from '@/assets/icon/common/CheckBox24Off.svg?react';
 
 interface ItemFormProps {
   mode: 'create' | 'edit';
@@ -36,10 +34,79 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
     useState(false);
   const [isEndDateTimeSheetOpen, setIsEndDateTimeSheetOpen] = useState(false);
 
-  const { watch, setValue } = useFormContext<ItemFormValues>();
+  const { control, setValue } = useFormContext<ItemFormValues>();
 
-  const hasStartDate = watch('hasStartDate');
+  // 1. startISODateTime 용 useController
+  const {
+    field: { value: startISODateTime, onChange: onStartISOChange },
+  } = useController({
+    name: 'startISODateTime',
+    control,
+  });
+
+  // 2. hasStartDate(“시작일이 없어요”) 용 useController
+  const {
+    field: { value: hasStartDate, onChange: onHasStartDateChange },
+  } = useController({
+    name: 'hasStartDate',
+    control,
+  });
+
+  // 3. endISODateTime 용 useController
+  const {
+    field: { value: endISODateTime, onChange: onEndISOChange },
+  } = useController({
+    name: 'endISODateTime',
+    control,
+  });
+  // 4. hasEndDate(“마감일이 없어요”) 용 useController
+  const {
+    field: { value: hasEndDate, onChange: onHasEndDateChange },
+  } = useController({
+    name: 'hasEndDate',
+    control,
+  });
+
+  const handleNoStartDate = () => {
+    if (startISODateTime) {
+      onStartISOChange(null);
+    } else if (hasStartDate && !startISODateTime) {
+      onHasStartDateChange(false);
+    } else {
+      onHasStartDateChange(true);
+      onStartISOChange(null);
+    }
+  };
+
+  const handleNoEndDate = () => {
+    if (endISODateTime) {
+      onEndISOChange(null);
+    } else if (hasEndDate && !endISODateTime) {
+      onHasEndDateChange(false);
+    } else {
+      onHasEndDateChange(true);
+      onEndISOChange(null);
+    }
+  };
+
   const handleSaveClick = () => {};
+
+  const formatDate = (dateString: string | null) => {
+    if (!dateString) return '달력에서 시작일과 시간을 선택하세요.';
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    });
+  };
+  useEffect(() => {
+    console.log('시작일:', startISODateTime);
+    console.log('시작일이 없어요:', hasStartDate);
+  }, [startISODateTime, hasStartDate]);
 
   return (
     <section className="box-border flex flex-col items-start gap-8">
@@ -68,7 +135,6 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
           name="titleText"
           maxLength={40}
           placeHolderContent="상품의 이름을 입력해 주세요."
-          isRequired={true}
         />
       </ItemSection>
 
@@ -88,20 +154,41 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
             <p className="caption-m">시작일</p>
             <div className="body2-m border-grey03 flex h-fit w-full items-center justify-between rounded-sm border px-3.5 py-2.5">
               <span className="text-grey06">
-                달력에서 시작일과 시간을 선택하세요.
+                {formatDate(startISODateTime)}
               </span>
               <CalendarIcon onClick={() => setIsStartDateTimeSheetOpen(true)} />
             </div>
           </div>
-          <div className="text-grey08 caption-m flex cursor-pointer items-center gap-[.375rem]">
-            {hasStartDate ? <CheckBoxOnIcon /> : <CheckBoxOffIcon />}
-            진행 중인 상품만 보기
+          {/* 시작 없어요 체크박스 */}
+          <div className="text-grey10 body2-m flex items-center gap-[0.5rem]">
+            <button onClick={handleNoStartDate} className="cursor-pointer">
+              {!startISODateTime && hasStartDate ? (
+                <CheckBoxOnIcon />
+              ) : (
+                <CheckBoxOffIcon />
+              )}
+            </button>
+            시작일이 없어요
           </div>
 
           {/* 마감일 */}
           <div className="flex w-full flex-col items-start justify-center gap-1.5">
             <p className="caption-m">마감일</p>
-            <SalePriceInput name="salePrice" />
+            <div className="body2-m border-grey03 flex h-fit w-full items-center justify-between rounded-sm border px-3.5 py-2.5">
+              <span className="text-grey06">{formatDate(endISODateTime)}</span>
+              <CalendarIcon onClick={() => setIsEndDateTimeSheetOpen(true)} />
+            </div>
+          </div>
+          {/* 마감일 없어요 체크박스 */}
+          <div className="text-grey10 body2-m flex items-center gap-[0.5rem]">
+            <button onClick={handleNoEndDate} className="cursor-pointer">
+              {!endISODateTime && hasEndDate ? (
+                <CheckBoxOnIcon />
+              ) : (
+                <CheckBoxOffIcon />
+              )}
+            </button>
+            마감일이 없어요
           </div>
         </div>
       </ItemSection>
@@ -112,7 +199,8 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
           isBottomSheetOpen={isStartDateTimeSheetOpen}
         >
           <DateTimePicker
-            name="startISODateTime"
+            dateTimeName="startISODateTime"
+            hasDateName="hasStartDate"
             type="시작일"
             onClose={() => setIsStartDateTimeSheetOpen(false)}
           />
@@ -126,7 +214,8 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
           isBottomSheetOpen={isEndDateTimeSheetOpen}
         >
           <DateTimePicker
-            name="endISODateTime"
+            dateTimeName="endISODateTime"
+            hasDateName="hasEndDate"
             type="마감일"
             onClose={() => setIsEndDateTimeSheetOpen(false)}
           />
@@ -142,7 +231,6 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
           name="summaryText"
           maxLength={18}
           placeHolderContent="제품 한 줄 소개를 입력해 주세요."
-          isRequired={true}
         />
       </ItemSection>
 
@@ -151,18 +239,18 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
         <div className="flex w-full flex-col items-start gap-3">
           <div className="flex w-full flex-col items-start justify-center gap-1.5">
             <p className="caption-m">정가</p>
-            <PriceInput name="price" />
+            <FormPriceInput name="price" />
           </div>
           <div className="flex w-full flex-col items-start justify-center gap-1.5">
             <p className="caption-m">할인가</p>
-            <SalePriceInput name="salePrice" />
+            <FormSalePriceInput name="salePrice" />
           </div>
         </div>
       </ItemSection>
 
       {/* 판매 링크 */}
       <ItemSection label="판매 링크">
-        <LinkInput name="linkText" />
+        <FormLinkInput name="linkText" />
       </ItemSection>
 
       {/* 진행 회차 */}
@@ -178,97 +266,97 @@ export const ItemForm = ({ mode }: ItemFormProps) => {
   );
 };
 
-/////////////
+// /////////////
 
-export const ItemRegistrationTest = () => {
-  const [titleText, setTitleText] = useState('');
-  const [linkText, setLinkText] = useState('');
-  const [commentText, setCommentText] = useState('');
-  const [images, setImages] = useState<string[]>([]);
-  const [price, setPrice] = useState<number | undefined>(undefined);
-  const [salePrice, setSalePrice] = useState<number | undefined>(undefined);
-  const [period, setPeriod] = useState<number | null>(null);
-  const [selectedCategoryList, setSelectedCategoryList] = useState<string[]>(
-    []
-  );
+// export const ItemRegistrationTest = () => {
+//   const [titleText, setTitleText] = useState('');
+//   const [linkText, setLinkText] = useState('');
+//   const [commentText, setCommentText] = useState('');
+//   const [images, setImages] = useState<string[]>([]);
+//   const [price, setPrice] = useState<number | undefined>(undefined);
+//   const [salePrice, setSalePrice] = useState<number | undefined>(undefined);
+//   const [period, setPeriod] = useState<number | null>(null);
+//   const [selectedCategoryList, setSelectedCategoryList] = useState<string[]>(
+//     []
+//   );
 
-  const [startDateTime, setStartDateTime] = useState<Date | null>(null);
-  const [endDateTime, setEndDateTime] = useState<Date | null>(null);
+//   const [startDateTime, setStartDateTime] = useState<Date | null>(null);
+//   const [endDateTime, setEndDateTime] = useState<Date | null>(null);
 
-  const [isStartDateTimeSheetOpen, setIsStartDateTimeSheetOpen] =
-    useState(false);
-  const [isEndDateTimeSheetOpen, setIsEndDateTimeSheetOpen] = useState(false);
+//   const [isStartDateTimeSheetOpen, setIsStartDateTimeSheetOpen] =
+//     useState(false);
+//   const [isEndDateTimeSheetOpen, setIsEndDateTimeSheetOpen] = useState(false);
 
-  const handleSaveClick = () => {};
-  useEffect(() => {
-    console.log(isStartDateTimeSheetOpen);
-  }, [isStartDateTimeSheetOpen]);
-  return (
-    <div className="box-border flex flex-col gap-y-10 px-5">
-      <CategoryChip text={'뷰티'} />
+//   const handleSaveClick = () => {};
+//   useEffect(() => {
+//     console.log(isStartDateTimeSheetOpen);
+//   }, [isStartDateTimeSheetOpen]);
+//   return (
+//     <div className="box-border flex flex-col gap-y-10 px-5">
+//       <CategoryChip text={'뷰티'} />
 
-      {/* 날짜 */}
-      {isStartDateTimeSheetOpen && (
-        <BottomSheet
-          onClose={() => setIsStartDateTimeSheetOpen(false)}
-          isBottomSheetOpen={isStartDateTimeSheetOpen}
-        >
-          <DateTimePicker
-            name="startISODateTime"
-            type="시작일"
-            onClose={() => setIsStartDateTimeSheetOpen(false)}
-          />
-        </BottomSheet>
-      )}
+//       {/* 날짜 */}
+//       {isStartDateTimeSheetOpen && (
+//         <BottomSheet
+//           onClose={() => setIsStartDateTimeSheetOpen(false)}
+//           isBottomSheetOpen={isStartDateTimeSheetOpen}
+//         >
+//           <DateTimePicker
+//             name="startISODateTime"
+//             type="시작일"
+//             onClose={() => setIsStartDateTimeSheetOpen(false)}
+//           />
+//         </BottomSheet>
+//       )}
 
-      {/* 날짜 */}
-      {isEndDateTimeSheetOpen && (
-        <BottomSheet
-          onClose={() => setIsEndDateTimeSheetOpen(false)}
-          isBottomSheetOpen={isEndDateTimeSheetOpen}
-        >
-          <DateTimePicker
-            name="endISODateTime"
-            type="마감일"
-            onClose={() => setIsEndDateTimeSheetOpen(false)}
-          />
-        </BottomSheet>
-      )}
+//       {/* 날짜 */}
+//       {isEndDateTimeSheetOpen && (
+//         <BottomSheet
+//           onClose={() => setIsEndDateTimeSheetOpen(false)}
+//           isBottomSheetOpen={isEndDateTimeSheetOpen}
+//         >
+//           <DateTimePicker
+//             name="endISODateTime"
+//             type="마감일"
+//             onClose={() => setIsEndDateTimeSheetOpen(false)}
+//           />
+//         </BottomSheet>
+//       )}
 
-      <SaveButton onClick={handleSaveClick} />
-      <div className="bg-grey01 flex h-[5.5rem] w-full shrink-0 items-center justify-center gap-[.4375rem] px-5 pt-[.4375rem] pb-8">
-        <DefaultButton
-          onClick={handleSaveClick}
-          disabled={false}
-          activeColor="bg-red-300"
-        />
-        <DefaultButton onClick={handleSaveClick} />
-      </div>
-      <LimitedTextInput
-        text={titleText}
-        setText={setTitleText}
-        maxLength={45}
-        placeHolderContent="상품의 이름을 입력해 주세요."
-        isRequired={true}
-      />
-      <LinkInput
-        text={linkText}
-        setText={setLinkText}
-        placeHolderContent="링크 URL을 입력해 주세요."
-        isRequired={true}
-      />
-      <WideTextArea
-        text={commentText}
-        setText={setCommentText}
-        placeHolderContent="제품 선택 이유, 특징, 사용 경험 등 제품의 매력을 보여줄 수 있는 내용을 자유롭게 작성해 주세요."
-        isRequired={true}
-      />
+//       <SaveButton onClick={handleSaveClick} />
+//       <div className="bg-grey01 flex h-[5.5rem] w-full shrink-0 items-center justify-center gap-[.4375rem] px-5 pt-[.4375rem] pb-8">
+//         <DefaultButton
+//           onClick={handleSaveClick}
+//           disabled={false}
+//           activeColor="bg-red-300"
+//         />
+//         <DefaultButton onClick={handleSaveClick} />
+//       </div>
+//       <LimitedTextInput
+//         text={titleText}
+//         setText={setTitleText}
+//         maxLength={45}
+//         placeHolderContent="상품의 이름을 입력해 주세요."
+//         isRequired={true}
+//       />
+//       <LinkInput
+//         text={linkText}
+//         setText={setLinkText}
+//         placeHolderContent="링크 URL을 입력해 주세요."
+//         isRequired={true}
+//       />
+//       <WideTextArea
+//         text={commentText}
+//         setText={setCommentText}
+//         placeHolderContent="제품 선택 이유, 특징, 사용 경험 등 제품의 매력을 보여줄 수 있는 내용을 자유롭게 작성해 주세요."
+//         isRequired={true}
+//       />
 
-      <TipTooltip
-        text={
-          '최대 3가지 카테고리를 선택할 수 있습니다!최대 3가지 카테고리를 선택할 수 있습니다!최대 3가지 카테고리를 선택할 수 있습니다!'
-        }
-      />
-    </div>
-  );
-};
+//       <TipTooltip
+//         text={
+//           '최대 3가지 카테고리를 선택할 수 있습니다!최대 3가지 카테고리를 선택할 수 있습니다!최대 3가지 카테고리를 선택할 수 있습니다!'
+//         }
+//       />
+//     </div>
+//   );
+// };
