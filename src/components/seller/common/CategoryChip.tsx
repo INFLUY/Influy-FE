5bc75ed114e4513545bec986b97405f565195ac8
seoyeon5117 <seoyun020207@gmail.com>
2025-06-19 14:32:39 +0900

    fix: fix conditional rendering for category chip styles

--- src/components/seller/common/CategoryChip.tsx
+++ src/components/seller/common/CategoryChip.tsx
@@ -20,13 +20,11 @@ const CategoryChip = ({
       className={cn(
         'body2-m flex h-fit w-fit cursor-pointer items-center justify-center',
         theme === 'item' && 'rounded-[1.25rem] px-2.5 py-1',
-        theme === 'item' && isSelected
-          ? 'bg-grey10 text-white'
-          : 'bg-grey03 text-black',
+        theme === 'item' &&
+          (isSelected ? 'bg-grey10 text-white' : 'bg-grey03 text-black'),
         theme === 'faq' && 'rounded-[2.5rem] px-3 py-[.375rem]',
-        theme === 'faq' && isSelected
-          ? 'bg-grey10 text-white'
-          : 'bg-grey01 text-black'
+        theme === 'faq' &&
+          (isSelected ? 'bg-grey10 text-white' : 'bg-grey01 text-black')
       )}
     >
       {text}
