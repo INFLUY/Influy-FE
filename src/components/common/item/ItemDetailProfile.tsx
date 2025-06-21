a8c27df6483db7625aaf1609a5f5de5cc24f3a7d
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-06-21 15:08:55 +0900

    fix: 판매자 프로필 label alt 수정

--- src/components/common/item/ItemDetailProfile.tsx
+++ src/components/common/item/ItemDetailProfile.tsx
@@ -6,12 +6,12 @@ export const ItemDetailProfile = ({ seller }: { seller: SellerCard }) => {
     <button
       className="bg-grey01 flex h-fit w-full cursor-pointer items-center justify-between px-5 py-3"
       type="button"
-      aria-label="판매자 프로필"
+      aria-label={seller.nickname + ' 프로필 바로가기'}
     >
       <div className="flex h-full w-full items-center gap-3">
         <img
           src={seller.profileImg}
-          alt="판매자 프로필"
+          alt={seller.nickname + ' 프로필 사진'}
           className="h-[3.75rem] w-[3.75rem] rounded-full object-cover"
         />
         <div className="flex flex-col text-left">
