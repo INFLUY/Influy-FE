de4658da37382296feb7d947e45e00a934b4089c
seoyeon5117 <seoyun020207@gmail.com>
2025-07-12 04:19:23 +0900

    feat: implement SignupEmailPage for seller

--- src/pages/auth/SignupEmailPage.tsx
+++ src/pages/auth/SignupEmailPage.tsx
@@ -1,5 +1,117 @@
+import { DefaultButton, PageHeader, SnackBar } from '@/components';
+import ArrowIcon from '@/assets/icon/common/ArrowIcon.svg?react';
+import XIcon from '@/assets/icon/common/XIcon.svg?react';
+import EmailIcon from '@/assets/icon/common/sns/EmailIcon.svg?react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '@/routes/path';
+import { TextInput } from '@/components/common/DetailInput';
+import { useSellerSignupStore, useUserSignupStore } from '@/store/authStore';
+import { emailSchema } from '@/schemas/profileSchema';
+
 export const SignupEmailPage = () => {
-  return <div className="flex h-full w-full flex-1 flex-col"></div>;
+  const navigate = useNavigate();
+
+  const [emailValue, setEmailValue] = useState<string>('');
+
+  const { reset: sellerSignupStateReset } = useSellerSignupStore();
+  const { reset: userSignupStateReset } = useUserSignupStore();
+  const [isDirty, setIsDirty] = useState(false); // 입력값이 한번이라도 바뀌었는지
+
+  const [snackbar, setSnackbar] = useState<{
+    open: boolean;
+    message: string;
+  }>({
+    open: false,
+    message: '',
+  });
+
+  const handleChangeEmail = (value: string) => {
+    if (!isDirty) setIsDirty(true);
+    setEmailValue(value);
+  };
+
+  // 다음 버튼 클릭 핸들러
+  const handleClickNext = () => {
+    if (!isDirty) setIsDirty(true);
+    const result = emailSchema.safeParse(emailValue);
+    if (!result.success) {
+      const message = result.error.issues.map((err) => err.message);
+      setSnackbar({
+        open: true,
+        message: message[0],
+      });
+    } else {
+      setEmailValue(emailValue);
+
+      // 백 연동
+
+      userSignupStateReset();
+      useUserSignupStore.persist.clearStorage();
+      sellerSignupStateReset();
+      useSellerSignupStore.persist.clearStorage();
+      navigate(PATH.WELCOME.base);
+    }
+  };
+
+  return (
+    <div className="flex h-full w-full flex-1 flex-col">
+      <PageHeader
+        leftIcons={[
+          <ArrowIcon
+            className="h-6 w-6 cursor-pointer text-black"
+            onClick={() => navigate(-1)}
+          />,
+        ]}
+        rightIcons={[
+          <XIcon
+            className="h-6 w-6 cursor-pointer text-black"
+            onClick={() => navigate('')}
+          />,
+        ]}
+      >
+        회원가입
+      </PageHeader>
+      <section className="flex w-full flex-1 flex-col gap-11 px-5 py-[3.25rem]">
+        <div className="flex w-full flex-col gap-2">
+          <h1 className="headline2 text-black">이메일을 입력해 주세요.</h1>
+          <p className="body1-m text-grey08 flex gap-[.9375rem] break-keep whitespace-break-spaces">
+            {`셀러 프로필의 이메일 아이콘에 등록됩니다.\n비즈니스 문의를 받고 싶은 이메일을\n등록해주세요.`}
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="body2-sb flex items-center gap-2 text-black">
+            <EmailIcon className="h-6 w-6" />
+            <span>이메일</span>
+          </div>
+          <TextInput
+            text={emailValue}
+            setText={handleChangeEmail}
+            isValid={!isDirty || emailSchema.safeParse(emailValue).success}
+            placeHolderContent="이메일 주소를 입력해 주세요."
+          />
+        </div>
+      </section>
+      <div className="sticky bottom-0 z-20 flex gap-[.4375rem] bg-white px-5 pt-[.625rem] pb-4">
+        <DefaultButton
+          type="button"
+          text="다음"
+          disabled={!emailValue}
+          useDisabled={false}
+          onClick={handleClickNext}
+        />
+      </div>
+
+      {/* 스낵바 */}
+      {snackbar.open && (
+        <SnackBar
+          handleSnackBarClose={() => setSnackbar({ open: false, message: '' })}
+        >
+          {snackbar.message}
+        </SnackBar>
+      )}
+    </div>
+  );
 };
 
 export default SignupEmailPage;
