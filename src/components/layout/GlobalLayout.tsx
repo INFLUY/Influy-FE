165d44b455e539b46e2446bdb21a983d19a0cf62
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-07-16 01:02:46 +0900

    fix: 핸드폰 크롬으로 접속시 h-screen으로 할 때 스크롤바 생기는 현상 고침(100dvh: visible viewport 기준의 높이 (주소창 제외))

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -1,6 +1,6 @@
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen w-screen max-w-[40rem] min-w-[20rem] flex-1 flex-col overflow-hidden bg-white md:w-[28rem]">
+    <div className="flex h-[100dvh] w-screen max-w-[40rem] min-w-[20rem] flex-1 flex-col overflow-hidden bg-white md:w-[28rem]">
       <main className="scrollbar-hide relative flex flex-1 flex-col overflow-y-auto">
         {children}
       </main>
