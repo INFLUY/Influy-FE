88da5273f31b50edd2f6f3f5f01c69b023d3e338
seoyeon5117 <seoyun020207@gmail.com>
2025-07-26 23:46:17 +0900

    feat: implement Global Modal using zustand

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -1,8 +1,11 @@
+import GlobalModal from '../common/globalModal';
+
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen w-screen max-w-[40rem] min-w-[20rem] flex-1 flex-col overflow-hidden bg-white md:w-[28rem]">
       <main className="scrollbar-hide relative flex flex-1 flex-col overflow-y-auto">
         {children}
+        <GlobalModal />
       </main>
     </div>
   );
