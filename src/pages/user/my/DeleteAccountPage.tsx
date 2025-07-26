d6d68081f329b350051f015bc40a84d7fbc30b9e
seoyeon5117 <seoyun020207@gmail.com>
2025-07-26 23:47:44 +0900

    feat: show modal when user clicks delete account button in delete page

--- src/pages/user/my/DeleteAccountPage.tsx
+++ src/pages/user/my/DeleteAccountPage.tsx
@@ -1,9 +1,25 @@
 import { DefaultButton, PageHeader } from '@/components';
 import { useNavigate } from 'react-router-dom';
 import ArrowIcon from '@/assets/icon/common/ArrowIcon.svg?react';
+import { useModalStore } from '@/store/useModalStore';
 
 const DeleteAccountPage = () => {
   const navigate = useNavigate();
+
+  const { showModal } = useModalStore();
+
+  const handleDeleteAccountClick = () => {
+    showModal({
+      text: `계정 탈퇴하시겠습니까?\n회원님의 모든 정보와 상품이 삭제되며\n되돌릴 수 없습니다.`,
+      leftButtonText: '취소',
+      rightButtonText: '탈퇴',
+      rightButtonClick: () => {
+        // TODO: 삭제 로직
+        console.log('탈퇴 완료');
+      },
+    });
+  };
+
   return (
     <div className="flex h-full w-full flex-1 flex-col pt-11">
       <PageHeader
@@ -24,7 +40,11 @@ const DeleteAccountPage = () => {
             복구할 수 없어요.
           </p>
         </div>
-        <DefaultButton activeTheme="grey" text="탈퇴하기" onClick={() => {}} />
+        <DefaultButton
+          activeTheme="grey"
+          text="탈퇴하기"
+          onClick={() => handleDeleteAccountClick()}
+        />
       </section>
     </div>
   );
