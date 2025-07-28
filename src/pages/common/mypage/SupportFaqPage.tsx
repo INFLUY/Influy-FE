85ee6e278cdb80b8c6d3a75e88977f40e0956b04
seoyeon5117 <seoyun020207@gmail.com>
2025-07-29 02:26:26 +0900

    feat: implement support faq page

--- src/pages/common/mypage/SupportFaqPage.tsx
+++ src/pages/common/mypage/SupportFaqPage.tsx
@@ -1,11 +1,15 @@
 import { PageHeader } from '@/components';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import ArrowIcon from '@/assets/icon/common/ArrowIcon.svg?react';
-import FAQ from '@/constants/supportFaq';
+import { USER_FAQ, SELLER_FAQ } from '@/constants/supportFaq';
+import { PATH } from '@/routes/path';
 
 const SupportFaqPage = () => {
   const navigate = useNavigate();
   const { supportFaqId } = useParams();
+  const { pathname } = useLocation();
+  const isSeller = pathname.includes(PATH.SELLER.BASE);
+  const faqList = isSeller ? SELLER_FAQ : USER_FAQ;
 
   return (
     <div className="flex h-full w-full flex-1 flex-col pt-11">
@@ -22,14 +26,14 @@ const SupportFaqPage = () => {
       <section className="flex flex-1 flex-col">
         <div className="border-b-grey02 flex flex-col gap-2 border-b px-5 py-6">
           <span className="body2-m text-grey07">
-            {FAQ[Number(supportFaqId)].category}
+            {faqList[Number(supportFaqId)].category}
           </span>
           <h1 className="subhead-sb text-black">
-            {FAQ[Number(supportFaqId)].question}
+            {faqList[Number(supportFaqId)].question}
           </h1>
         </div>
         <p className="body2-r flex w-full flex-1 px-5 py-6 whitespace-pre-wrap text-black">
-          {FAQ[Number(supportFaqId)].answer}
+          {faqList[Number(supportFaqId)].answer}
         </p>
       </section>
     </div>
