a3b48395c0d9d00aa56226a6fa203e207a9d5afa
seoyeon5117 <seoyun020207@gmail.com>
2025-08-06 23:13:16 +0900

    feat: send file extension when uploading via presigned URL

--- src/api/presignedUrl/putPresignedUrl.ts
+++ src/api/presignedUrl/putPresignedUrl.ts
@@ -4,20 +4,11 @@ export const putPresignedUrl = async (
   presignedUrl: string,
   file: File
 ): Promise<void> => {
-  // 임시
-  const fixJpgMimeType = (file: File): File => {
-    const ext = file.name.split('.').pop()?.toLowerCase();
-    if (ext === 'jpg' && file.type === 'image/jpeg') {
-      return new File([file], file.name, { type: 'image/jpg' });
-    }
-    return file;
-  };
-
-  const fixedFile = fixJpgMimeType(file);
+  const fixedType = 'image/' + file.name.split('.').pop();
 
   const response = await axios.put(presignedUrl, file, {
     headers: {
-      'Content-Type': fixedFile.type,
+      'Content-Type': fixedType,
     },
   });
 
