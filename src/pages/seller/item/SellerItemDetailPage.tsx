cda9ca9e71fa0c19d7aec6a18c4eb94e3eb4b744
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-08-06 14:22:49 +0900

    feat: faq 카드 없을 때

--- src/pages/seller/item/SellerItemDetailPage.tsx
+++ src/pages/seller/item/SellerItemDetailPage.tsx
@@ -297,8 +297,10 @@ const SellerItemDetailPage = () => {
             </article>
           </div>
         </article>
+
+        {/* faq 카드 */}
         <Suspense fallback={<LoadingSpinner />}>
-          {flattenedFaqCardList && (
+          {flattenedFaqCardList && flattenedFaqCardList.length > 0 ? (
             <ItemDetailFaqCard
               totalElements={
                 faqCardList?.pages[faqCardList.pages.length - 1]
@@ -309,6 +311,14 @@ const SellerItemDetailPage = () => {
               isFetchingNextPage={isFetchingNextPage}
               fetchNextPage={fetchNextPage}
             />
+          ) : (
+            <div className="flex h-[34.125rem] w-full justify-center pt-15">
+              <p className="body2-m text-grey09 text-center">
+                아직 해당 카테고리에
+                <br />
+                FAQ가 등록되지 않았어요.
+              </p>
+            </div>
           )}
         </Suspense>
       </section>
